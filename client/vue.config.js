17b805472204eb4d38062b3edf41f998c252f241
unknown <hohmann.lorenz99@gmail.com>
2019-04-11 20:08:43 +0200

    tried to fix proxy problems on live version

--- client/vue.config.js
+++ client/vue.config.js
@@ -4,10 +4,7 @@ module.exports = {
 	outputDir: path.resolve(__dirname, '../server/public'),
 	devServer: {
 		proxy: {
-			'/api/game': {
-				target: 'http://localhost:3000'
-			},
-			'/api/player': {
+			'/api/*': {
 				target: 'http://localhost:3000'
 			}
 		}
