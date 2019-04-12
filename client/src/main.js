5bed3f6e4aa1f25cee69e456513f60b6403ac47f
unknown <hohmann.lorenz99@gmail.com>
2019-04-12 18:23:38 +0200

    socket problem fix #2

--- client/src/main.js
+++ client/src/main.js
@@ -10,7 +10,7 @@ Vue.config.productionTip = false;
 Vue.use(
 	new VueSocketIO({
 		debug: true,
-		connection: 'http://localhost',
+		connection: '//localhost',
 		vuex: {
 			store,
 			actionPrefix: 'SOCKET_',
