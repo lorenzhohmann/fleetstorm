b9297b035078d12cd074903db5aaef635051b953
unknown <hohmann.lorenz99@gmail.com>
2019-04-12 18:25:32 +0200

    socket problem fix #3

--- client/src/main.js
+++ client/src/main.js
@@ -10,7 +10,7 @@ Vue.config.productionTip = false;
 Vue.use(
 	new VueSocketIO({
 		debug: true,
-		connection: '//localhost',
+		connection: 'https://battleship.lorenzhohmann.de/',
 		vuex: {
 			store,
 			actionPrefix: 'SOCKET_',
